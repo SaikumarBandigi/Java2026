679ee6300572299cc36eb2e7eb7f18340710411a
Saikumar Bandigi <bandigisaikumar@gmail.com>
2026-04-11 11:36:08 +0530

    added Groupthembasedonwhethertheystartwithavoweloraconsonant File

---
+++ src/streamapitut/pack1/Groupthembasedonwhethertheystartwithavoweloraconsonant.java
@@ -0,0 +1,38 @@
+package streamapitut.pack1;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class Groupthembasedonwhethertheystartwithavoweloraconsonant {
+
+    public static void main(String[] args) {
+
+        List<String> words = Arrays.asList("apple", "banana", "orange", "grape", "umbrella", "cat");
+
+        Function<String, String> function = new Function<>() {
+            @Override
+            public String apply(String s) {
+                if (isVowel(s.charAt(0))) {
+                    return "VOWEL";
+                }
+                return "CONSONANT";
+            }
+        };
+
+        Map<String, List<String>> map = words.stream().collect(Collectors.groupingBy(function));
+
+        for (String s : map.keySet()) {
+            System.out.println(s + " " + map.get(s));
+        }
+
+
+    }
+
+    static boolean isVowel(char ch) {
+        return "aeiouAEIOU".indexOf(ch) != -1;
+    }
+
+}
