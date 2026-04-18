cd6f25bda9bc43b3a2278082c480154913cadc53
Saikumar Bandigi <bandigisaikumar@gmail.com>
2026-04-18 23:10:44 +0530

    updated Example File

--- src/streamapitut/pack2/Example.java
+++ src/streamapitut/pack2/Example.java
@@ -7,12 +7,16 @@ public class Example {
 
     public static void main(String[] args) {
 
-
         List<Integer> list = Arrays.asList(5, 12, 8, 20, 3);
 
         list.stream()
                 .filter(x -> x > 10)
                 .forEach(System.out::println);
+
+
+        List<String> stringList = Arrays.asList("sai", "kumar", "bandigi");
+        stringList.stream().map(String::toUpperCase).forEach(System.out::println);
+
 
     }
 
